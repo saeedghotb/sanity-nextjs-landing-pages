a0f41292f0c936a9d93488aba2bc4fbb219a93f9
Saeed Ghotb <saeedghotb95@gmail.com>
2021-02-07 11:52:28 +0330

    remove comment

--- web/client.js
+++ web/client.js
@@ -1,9 +1,9 @@
-const sanityClient = require('@sanity/client')
+const sanityClient = require("@sanity/client");
 const client = sanityClient({
-  projectId: 'co0n96cc',
-  dataset: 'production',
-  token: '', // or leave blank to be anonymous user
-  useCdn: false // `false` if you want to ensure fresh data
-})
+  projectId: "co0n96cc",
+  dataset: "production",
+  token: "", // or leave blank to be anonymous user
+  useCdn: false,
+});
 
-module.exports = client
+module.exports = client;
